1cde1cd081305b40f46a1f7539b36ba63ca44b74
Paul Cushing <paul.cushing@clickbank.com>
2018-11-19 11:38:39 -0700

    Hiding search box until after auth with Spotify

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
+    let buttonText = this.props.auth ? 'SEARCH' : 'LOGIN TO SPOTIFY';
+    let inputStatus = this.props.auth ? '' : 'hidden';
+
     return (
       <div className="SearchBar">
         <input
@@ -9,8 +12,9 @@ class SearchBar extends Component {
           type="text"
           value={this.props.term}
           onChange={this.props.onChange}
+          className={inputStatus}
         />{' '}
-        <a onClick={this.props.onClick}>SEARCH</a>
+        <a onClick={this.props.onClick}>{buttonText}</a>
       </div>
     );
   }
